dcb846741e58091da90c267d6de2bdc394bb11b7
abolfazl <abazsh3@gmail.com>
2019-07-18 15:59:57 +0430

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const socketIo = require('socket.io');
+const http = require('http');
 let express = require('express');
-let publicPath=path.join(__dirname,"../public");
-let app =express();
+let publicPath = path.join(__dirname, "../public");
+let app = express();
+let server = http.createServer(app);
+let io=socketIo(server);
 
 app.use(express.static(publicPath));
-app.listen(process.env.PORT || 3000, function(){
+io.on("connection",(socket)=>{
+   console.log("new user connected");
+   socket.on("disconnect",()=>{
+       console.log("user disconnected");
+   });
+});
+server.listen(process.env.PORT || 3000, function () {
     console.log('listening on', process.env.PORT || 3000);
 });
