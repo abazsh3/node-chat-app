1997c96be28a93096e566bd3d82bc237fe865e19
abolfazl <abazsh3@gmail.com>
2019-07-19 02:45:08 +0430

    socket.io

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+let socket = io();
+socket.on("connect",function(){
+    console.log("connected to server")
+});
+socket.on("disconnect",function(){
+    console.log("disconnected");
+});
+socket.on("newMessage",function (message) {
+    console.log("new email",message);
+});
+
+
--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ let io=socketIo(server);
 app.use(express.static(publicPath));
 io.on("connection",(socket)=>{
    console.log("new user connected");
+   socket.on("createMessage",(message)=>{
+       console.log(message);
+       io.emit("newMessage",{
+           from:message.from,
+           text:message.text,
+           createdAt:new Date().getTime()
+       })
+   });
    socket.on("disconnect",()=>{
        console.log("user disconnected");
    });
