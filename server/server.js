10f41afd9fad0946a5018ec45316a10602e733ea
abolfazl <abazsh3@gmail.com>
2019-07-19 03:11:00 +0430

    messaging system is ready

--- server/server.js
+++ server/server.js
@@ -10,13 +10,29 @@ let io=socketIo(server);
 app.use(express.static(publicPath));
 io.on("connection",(socket)=>{
    console.log("new user connected");
+    socket.emit("newMessage",{
+        from: "Admin",
+        text: "welcome to chat room",
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit("newMessage",{
+        from:"Admin",
+        text:"new user joined",
+        createdAt: new Date().getTime()
+    });
    socket.on("createMessage",(message)=>{
        console.log(message);
+
        io.emit("newMessage",{
            from:message.from,
            text:message.text,
            createdAt:new Date().getTime()
        })
+       // socket.broadcast.emit("newMessage",{
+       //     from:message.from,
+       //     text:message.text,
+       //     createdAt:new Date().getTime()
+       // })
    });
    socket.on("disconnect",()=>{
        console.log("user disconnected");
