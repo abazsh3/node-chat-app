7631869869891d0b1508df410ecf9c0158d29bed
abolfazl <abazsh3@gmail.com>
2019-07-21 21:13:40 +0430

    bugs fixed

--- server/server.js
+++ server/server.js
@@ -10,16 +10,18 @@ let app = express();
 let server = http.createServer(app);
 let io = socketIo(server);
 let users = new Users();
+let userNameList=[];
 app.use(express.static(publicPath));
+
 io.on("connection", (socket) => {
     console.log("new user connected");
-
     socket.on("createMessage", function(message, callback){
+        console.log('message is creating');
         let user=users.getUser(socket.id);
-        if (user&&isRealString(message)) {
+        if (user && isRealString(message.text)) {
             io.to(user.room).emit("newMessage", generateMessage(user.name, message.text));
+            console.log('message emitted');
         }
-
 
         try {
             callback();
@@ -28,11 +30,12 @@ io.on("connection", (socket) => {
         }
     });
     socket.on('join', function(params, callback){
+
         if (!isRealString(params.name) || !isRealString(params.room)) {
             return callback('name and room name are required!')
         }
-        socket.join(params.room);
         users.removeUser(socket.id);
+        socket.join(params.room);
         users.addUser(socket.id, params.name, params.room);
         io.to(params.room).emit("updateUsersList", users.getUserList(params.room));
         socket.emit("newMessage", generateMessage("Admin", "welcome to chat app"));
